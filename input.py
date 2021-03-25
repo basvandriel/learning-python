dd3a4bcfecb89e1a9befe63f3ce9a759494f4a6e
Bas van Driel <contact@basvandriel.nl>
2021-03-25 14:27:57 +0100

    Math stuff.

---
+++ input.py
@@ -0,0 +1,6 @@
+# User input
+
+name = input('Name: ')
+age = input('Age: ')
+
+print('Hello,', name, '. You are', age, 'years old')
---
+++ math.py
@@ -0,0 +1,15 @@
+# Math stuff
+x = 9
+y = 3.5
+
+result = x * y + y / x
+
+print(result)
+
+# Int conversion
+result = int(result)
+print(result)
+
+# **, //
+
+# Order of operations:  rackets (...), exponants, division /, multiplication *, addition +, substraction -
