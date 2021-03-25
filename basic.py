02158b0e1a9503041d5ba3a461966a757e26d764
Bas van Driel <contact@basvandriel.nl>
2021-03-25 14:18:04 +0100

    first commit

---
+++ basic.py
@@ -0,0 +1,33 @@
+# Basic Python operations
+
+# Int
+-123123
+
+# Float
+123.123
+
+# String
+'Duh'
+"Duhh"
+
+# Boolean
+True
+False
+1
+0
+
+# Terminal output
+print("Hello World!")
+print(4.5)
+
+print(5.7, "Hey, it's with space")
+print(5.7, "Hey, it's with space and custom ending", end="|")
+
+# Variables
+hello = 'Bas'
+world = 'world'
+print(hello, world)
+
+world = hello
+
+print(hello, world)
