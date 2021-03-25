3436f5aee2f48c6c6b2cf8825032878b8890d1de
Bas van Driel <contact@basvandriel.nl>
2021-03-25 15:20:54 +0100

    Loops

---
+++ loops.py
@@ -0,0 +1,26 @@
+
+# Iterator i
+for i in range(10):
+    print(i)
+
+for i in range(1, 10):
+    print(i)
+
+# Range: start, stop, step
+
+# In list
+for i in [1, 2, 3, 4]:
+    print(i)
+
+# Enumerate
+elements = [1, 'Hey', 42, True, 'No', 'Stop']
+
+for index, element in enumerate(elements):
+    print(i, element)
+
+# While loops
+
+condition = 1 < 2
+while condition == True:
+    print('Yes!')
+    break
